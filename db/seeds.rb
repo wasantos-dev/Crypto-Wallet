43d8a6eb7ab58c6766329cd758a8a87772794ad1
Wagner Cavalcante Santos <wasantos@bionexo.com>
2020-01-21 22:16:22 -0300

    Refactor rails tasks and add spinner in project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+spinner = TTY::Spinner.new("[:spinner] Criando Moedas")	
+spinner.auto_spin 
+
+coins = [
+					{
+						description: "Bitcoin",
+						acronym: "BTC",
+						url_image: "https://imagepng.org/wp-content/uploads/2017/06/moeda-bitcoin-coin.png"
+					},
+					{
+						description: "Ethereum",
+						acronym: "ETH",
+						url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png"
+					},
+					{
+						description: "Dash",
+						acronym: "DASH",
+						url_image: "https://images.cointelegraph.com/images/240_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS9zdG9yYWdlL3VwbG9hZHMvdmlldy82YzlhZTM1NGQwY2E4OTdlNTU1OThlZTZhOGJmM2UxMC5wbmc=.png"
+					}
+				]
+
+coins.each do |coin|
+	Coin.find_or_create_by!(coin)
+end
+
+spinner.success("(Concluído com Sucesso)")
---
+++ lib/tasks/dev.rake
@@ -0,0 +1,22 @@
+namespace :dev do
+  desc "Configurando o ambiente de desenvolvimento"
+  task setup: :environment do
+  	if Rails.env.development?
+  		show_spinner("Apagando DB...") { %x(rails db:drop) }
+  		show_spinner("Criando DB...") { %x(rails db:create) }
+  		show_spinner("Migrando DB...") { %x(rails db:migrate) }
+  		show_spinner("Populando DB...") { %x(rails db:seed) }
+  	else
+  		puts "Você não está em ambiente de desenvolvimento"
+  	end
+  end
+
+  private
+
+	def show_spinner(msg_start, msg_end = "Concluído!")
+		spinner = TTY::Spinner.new("[:spinner] #{msg_start}")	
+		spinner.auto_spin 
+		yield
+		spinner.success("(#{msg_end})")
+	end
+end
